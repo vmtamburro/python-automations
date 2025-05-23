655d1c5b3f674fe9a8dd0cb16e85abaf989d5551
Victoria Tamburro <43652134+vmtamburro@users.noreply.github.com>
2025-05-23 10:06:29 -0400

    Update create_conda.sh

--- bash_scripts/create_conda.sh
+++ bash_scripts/create_conda.sh
@@ -10,5 +10,8 @@ conda env config vars set SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
 
 pip install -r requirements/dev.txt
 
+pip install pyfarmhash==0.3.2;
+
 # https://stackoverflow.com/questions/54499749/how-to-fix-the-tensorflow-library-was-compiled-to-use-avx512f-instructions-but
 pip uninstall -y numpy tensorflow tensorflow-macos tensorflow-decision-forests && pip install tensorflow-macos 
+
